30b5fe7c07a9b3b5111e194808e9fc90f329476c
Jonathan Foucher <jonathan-foucher@outlook.fr>
2022-02-13 16:48:19 +0100

    add linter

--- http-server.js
+++ http-server.js
@@ -1,5 +1,7 @@
 const express = require('express');
+
 const bodyParser = require('body-parser');
+
 const port = process.env.PORT;
 
 const app = express();
@@ -7,7 +9,7 @@ app.use(bodyParser.json());
 
 app.post('/data', (req, res) => {
   // TODO manage received POST here
-  console.log("POST received");
+  console.log('POST received');
   console.log(JSON.stringify(req.headers));
   console.log(req.body);
   res.end();

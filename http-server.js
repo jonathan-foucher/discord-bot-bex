4c74f524b13bf58a22f23c8110a246da9c20eecb
Jonathan Foucher <jonathan-foucher@outlook.fr>
2022-02-13 16:33:17 +0100

    listen to http requests

---
+++ http-server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const port = process.env.PORT;
+
+const app = express();
+app.use(bodyParser.json());
+
+app.post('/data', (req, res) => {
+  // TODO manage received POST here
+  console.log("POST received");
+  console.log(JSON.stringify(req.headers));
+  console.log(req.body);
+  res.end();
+});
+
+app.listen(port, () => {
+  console.log(`Application is listening on port ${port}`);
+});
--- main.js
+++ main.js
@@ -1,2 +1,2 @@
 require('dotenv').config();
-
+require('./http-server.js');
